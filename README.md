d20634ce978dfad8b2840e40a9d865e242bada38
red-ornob <red.ornob.dev@gmail.com>
2025-06-30 19:37:06 +0600

    changed copyright comments

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ This is a note-taking app integrating Markdown and MathJax together, written in 
 
 HEAVILY WIP right now.
 
-Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)\
+Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)
+This program is subject to the AGPL-3.0-or-later licence.\
 Check NOTICE.md at the project root for the full notice.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-// Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)
-// Check NOTICE.md at the project root for the full notice.
+/*
+ * Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)
+ * This program is subject to the AGPL-3.0-or-later licence.
+ * Check NOTICE.md at the project root for the full notice.
+ */
 
-nw.Window.open("index.html", {}, function(win) {})
+nw.Window.open("index.html")
