f61b63a39b7733d898f4c4e8ec9101dcb6bfe61e
red-ornob <red.ornob.dev@gmail.com>
2025-07-02 17:06:14 +0600

    mathjax setup done it took sooooo long...

---
+++ src/scripts/mathjax.js
@@ -0,0 +1,26 @@
+// Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)
+// This program is subject to the AGPL-3.0-or-later licence.
+// Check NOTICE.md at the project root for the full notice.
+
+window.MathJax = {
+    tex: {
+        inlineMath: [
+            ["\\(", "\\("],
+            ["$`", "`$"]
+        ],
+        displayMath: [
+            ["\\[", "\\]"],
+            ["```math", "```"]
+        ]
+    },
+    options: {
+        enableMenu: false
+    }
+};
+
+(function () {
+    let script = document.createElement("script");
+    script.src = "libs/tex-svg-full.js";
+    script.async = true;
+    document.head.appendChild(script);
+})();
