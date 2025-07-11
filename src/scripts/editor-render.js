b1aaf6a0c501772f3bebd41333b5f016e319228d
red-ornob <red.ornob.dev@gmail.com>
2025-07-11 19:42:20 +0600

    WIP editor and viewer pages

--- src/scripts/editor-render.js
+++ src/scripts/editor-render.js
@@ -15,16 +15,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const viewer = document.querySelector('#viewer');
     // buffer used to remove flickering caused by unrendered markdown and mathjax
     const buffer = document.querySelector('#buffer');
-
+    
     let toRender = false;
-
+    
     editor.addEventListener('input', function () {
         buffer.innerHTML = sanitizeHtml(editor.value);
         toRender = true;
     });
-
+    
     setInterval(function render() { // to limit the number of calls
-        if (toRender) { // to only call when there is a change
+        if (toRender) {
             // noinspection JSUnresolvedReference
             MathJax.typeset([buffer]); // typeset is not detected as a function
             
---
+++ src/scripts/viewer-render.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2025 [red.ornob](https://github.com/red-ornob) [<red.ornob.dev@gmail.com>](mailto:red.ornob.dev@gmail.com)
+ * This program is subject to the AGPL-3.0-or-later licence.
+ * Check NOTICE.md at the project root for the full notice.
+ */
+
+'use strict';
+
+const sanitizeHtml = require("./lib/sanitize-html.js");
+import { marked } from "./markdown.js"
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const text = "WIP: get text from whatever filesystem implementation";
+    const viewer = document.querySelector('#viewer');
+    // buffer used to remove flickering caused by unrendered markdown and mathjax
+    const buffer = document.querySelector('#buffer');
+    
+    document.addEventListener('DOMContentLoaded', function () { // need to change the event to appropriate event
+        buffer.innerHTML = sanitizeHtml(text);
+       
+        // noinspection JSUnresolvedReference
+        MathJax.typeset([buffer]); // typeset is not detected as a function
+        
+        buffer.innerHTML = marked.parse(buffer.innerHTML);
+        
+        viewer.innerHTML = buffer.innerHTML;
+    });
+});
