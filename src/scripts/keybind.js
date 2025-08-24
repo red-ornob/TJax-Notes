ef1108c47c08cca09b18770ae441026410360351
red-ornob <red.ornob.dev@gmail.com>
2025-08-24 17:26:48 +0600

    added a save and open file function/keybind

--- src/scripts/keybind.js
+++ src/scripts/keybind.js
@@ -6,48 +6,47 @@
 
 'use strict';
 
+const fs =  require('fs').promises;
+const path = require('path');
 import config from "./userdata.js";
-console.log(config);
+
+const page = document.URL.split("/").pop().split(".")[0];
+
+if (page === "editor") {
+    bind({
+        code: "KeyV",
+        ctrl: false,
+        shift: false,
+        alt: true,
+    }, toggleViewer);
+
+    bind({
+        code: "KeyS",
+        ctrl: true,
+        shift: false,
+        alt: false,
+    }, saveFile);
+
+    bind({
+        code: "KeyO",
+        ctrl: true,
+        shift: false,
+        alt: false,
+    }, openFile);
+}
 
 function bind({code, ctrl, shift, alt}, func) {
     document.addEventListener('keyup',
         (e) => {
-            if ((!ctrl || e.ctrlKey) && 
+            if ((!ctrl || e.ctrlKey) &&
                 (!shift || e.shiftKey) &&
-                (!alt || e.altKey) && 
+                (!alt || e.altKey) &&
                 e.code === code) {
                 func()
             }
         },
         false);
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const page = document.URL.split("/").pop().split(".")[0];
-    
-    if (page === "editor") {
-        bind({
-            code: "KeyV",
-            ctrl: false,
-            shift: false,
-            alt: true,
-        }, toggleViewer);
-
-        bind({
-            code: "KeyS",
-            ctrl: true,
-            shift: false,
-            alt: false,
-        }, saveFile);
-
-        bind({
-            code: "KeyO",
-            ctrl: true,
-            shift: false,
-            alt: false,
-        }, openFile);
-    }
-})
 
 function toggleViewer() {
     const viewer = document.querySelector('#viewer');
@@ -61,10 +60,32 @@ function toggleViewer() {
     }
 }
 
-function saveFile() {
-    alert("TODO")
+async function saveFile() {
+    const content = document.querySelector('#editor').value;
+    const filename = document.querySelector('#title').value;
+    
+    if (!filename) {
+        alert("Note needs a name.")
+        return;
+    }
+    
+    const filepath = path.join(config.notesDir, filename + ".tjx");
+    await fs.writeFile(filepath, content, "utf8")
 }
 
 function openFile() {
-    alert("TODO")
+    const editor = document.querySelector('#editor');
+    const title = document.querySelector('#title');
+    
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.tjx,.txt';
+    
+    input.onchange = async (event) => {
+        const file = event.target.files[0];
+        title.value = file.name.split(".")[0];
+        editor.value = await fs.readFile(file.path, "utf8");
+    };
+    
+    input.click();
 }
